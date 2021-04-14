2f26b3f78aa4944dbca3810b53296a80010ab888
YagovkinSI <sorpionupgrade@mail.ru>
2021-04-14 06:10:00 +0400

    Added map

--- YSI.CurseOfSilverCrown.Web/Controllers/HomeController.cs
+++ YSI.CurseOfSilverCrown.Web/Controllers/HomeController.cs
@@ -53,6 +53,11 @@ namespace YSI.CurseOfSilverCrown.Web.Controllers
             return View();
         }
 
+        public IActionResult Map()
+        {            
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
