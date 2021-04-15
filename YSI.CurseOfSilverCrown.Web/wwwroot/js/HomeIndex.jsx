35f3a75bd9164c03b64e20d9be4afeaee2bc0216
SamheinDM <SamheinDM@yandex.ru>
2021-04-15 22:14:06 +0300

    Fixing functions imports, remove old components

--- YSI.CurseOfSilverCrown.Web/wwwroot/js/HomeIndex.jsx
+++ YSI.CurseOfSilverCrown.Web/wwwroot/js/HomeIndex.jsx
@@ -1,62 +1,51 @@
-export default class HomeIndex extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          turnName: "",
-          lastEventStories: [[]]
-      };
-  }
-  loadData() {
-      var xhr = new XMLHttpRequest();
-      xhr.open("get", "api/Home/Index", true);
-      xhr.onload = function () {
-          let data = JSON.parse(xhr.responseText);
-          this.setState({
-              turnName: data.turn,
-              lastEventStories: data.lastEventStories
-          });
-      }.bind(this);
-      xhr.send();
-  }
-  componentDidMount() {
-      this.loadData();
-  }
-  render() {
-      return <>
-          <div className="text-center">
-              <h1 className="display-4">Добро пожаловать</h1>
+export default function HomeIndex () {
+  const [turnName, setTurnName] = React.useState('');
+  const [lastEventStories, setLastEventStories] = React.useState([[]]);
 
-              <p>Узнай о том как <a href="https://vk.com/club189975977">разрабатывается игра</a>.</p>
+  React.useEffect(() => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "api/Home/Index", true);
+    xhr.onload = () => {
+        let data = JSON.parse(xhr.responseText);
+        setTurnName(data.turn);
+        setLastEventStories(data.lastEventStories);
+    };
+    xhr.send();
+  }, []);
 
-              <h3>На дворе {this.state.turnName}</h3>
-
-              {/*@{}*/}
-              {/*        var user = UserManager.GetUserAsync(User).Result;*/}
-              {/*        var isAdmin = user != null*/}
-              {/*        ?UserManager.IsInRoleAsync(user, "Admin").Result*/}
-              {/*        : false;*/}
-              {/*        if (isAdmin)*/}
-              {/*        {<a className="nav-link text-dark" asp-area="" asp-controller="Admin" asp-action="NextTurn">Закончить ход</a>}*/}
-              {/*    }*/}
-          </div>
-          <div>
-              <h4>
-                  Важнейшие события прошлого
-              </h4>
-              {/*{*/}
-              {/*    this.state.lastEventStories.map(function (eventStory) {*/}
-              {/*        return <>*/}
-              {/*            {*/}
-              {/*                eventStory.map(function (line) {*/}
-              {/*                    return*/}
-              {/*                    <p style="margin-bottom: 0;">{line}</p>*/}
-              {/*                })*/}
-              {/*            }*/}
-              {/*            <p style="margin-bottom: 0;" > ____________________</p>*/}
-              {/*        </>*/}
-              {/*    })*/}
-              {/*}*/}
-          </div>
-      </>
-  }
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="display-4">Добро пожаловать</h1>
+        <p>Узнай о том как <a href="https://vk.com/club189975977">разрабатывается игра</a>.</p>
+        <h3>На дворе {turnName}</h3>
+        {/*@{}*/}
+        {/*        var user = UserManager.GetUserAsync(User).Result;*/}
+        {/*        var isAdmin = user != null*/}
+        {/*        ?UserManager.IsInRoleAsync(user, "Admin").Result*/}
+        {/*        : false;*/}
+        {/*        if (isAdmin)*/}
+        {/*        {<a className="nav-link text-dark" asp-area="" asp-controller="Admin" asp-action="NextTurn">Закончить ход</a>}*/}
+        {/*    }*/}
+      </div>
+      <div>
+        <h4>
+          Важнейшие события прошлого
+        </h4>
+        {/*{*/}
+        {/*    this.state.lastEventStories.map(function (eventStory) {*/}
+        {/*        return <>*/}
+        {/*            {*/}
+        {/*                eventStory.map(function (line) {*/}
+        {/*                    return*/}
+        {/*                    <p style="margin-bottom: 0;">{line}</p>*/}
+        {/*                })*/}
+        {/*            }*/}
+        {/*            <p style="margin-bottom: 0;" > ____________________</p>*/}
+        {/*        </>*/}
+        {/*    })*/}
+        {/*}*/}
+      </div>
+    </div>
+  );
 }
--- YSI.CurseOfSilverCrown.Web/wwwroot/js/LoginPartial.jsx
+++ YSI.CurseOfSilverCrown.Web/wwwroot/js/LoginPartial.jsx
@@ -1,38 +1,30 @@
-export default class LoginPartial extends React.Component {   
-  constructor(props) {
-      super(props);
-      this.onLoginClick = this.onLoginClick.bind(this);
-      this.onLogoutClick = this.onLogoutClick.bind(this);
-  }
-  onLoginClick() {
-      this.props.changeMainPage("login");
-  }
-  onLogoutClick() {
-      this.props.onUserLogout();
-  }
-  render() {
-      if (this.props.currentUser.isSignedIn) {
-          return <ul className="navbar-nav">
-              <li className="nav-item">
-                  <a className="nav-link text-dark" > {/*asp-area="Identity" asp-page="/Account/Manage/Index" title="Manage"*/}
-                      Здравствуйте, {this.props.currentUser.userName}!
-                  </a>
-              </li>
-              <li className="nav-item">
-                  <form className="form-inline">
-                      <button className="nav-link btn btn-link text-dark" onClick={this.onLogoutClick}>Выйти</button>
-                  </form>
-              </li>
-          </ul >
-      } else {
-          return <ul className="navbar-nav">                
-              {/*<li className="nav-item">*/}
-              {/*    <a className="nav-link text-dark" asp-area="Identity" asp-page="/Account/Register">Регистрация</a>*/}
-              {/*</li>*/}
-              <li className="nav-item">
-                  <a className="nav-link text-dark" onClick={this.onLoginClick}>Вход</a>
-              </li>
-          </ul >;
-      }        
-  }
+export default function LoginPartial(props) {
+  if (props.currentUser.isSignedIn) {
+    return (
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link text-dark" > {/*asp-area="Identity" asp-page="/Account/Manage/Index" title="Manage"*/}
+            Здравствуйте, {props.currentUser.userName}!
+          </a>
+        </li>
+        <li className="nav-item">
+          <form className="form-inline">
+            <button className="nav-link btn btn-link text-dark" onClick={props.onUserLogout}>Выйти</button>
+          </form>
+        </li>
+      </ul >
+    );
+  } else {
+    return (
+      <ul className="navbar-nav">
+        {/*<li className="nav-item">*/}
+        {/*    <a className="nav-link text-dark" asp-area="Identity" asp-page="/Account/Register">Регистрация</a>*/}
+        {/*</li>*/}
+        <li className="nav-item">
+          <a className="nav-link text-dark" onClick={() => props.changeMainPage('login')}>Вход</a>
+        </li>
+      </ul >
+    );
+  }        
+
 }
