57f84b6edd9d334cfad0653a6d1772599a6f4cc0
YagovkinSI <sorpionupgrade@mail.ru>
2023-04-01 09:00:12 +0400

    refactor - UI - Use React.FC for Home page

--- YSI.CurseOfSilverCrown.Web/ClientApp/src/components/Home.tsx
+++ YSI.CurseOfSilverCrown.Web/ClientApp/src/components/Home.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
 
-const Home = () => (
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
-);
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Главная страница</h1>
+    </div>
+  );
+}
 
-export default connect()(Home);
+export default Home;
