8b800f35688b703adebcf109493b74f5a87e87ae
YagovkinSI <sorpionupgrade@mail.ru>
2023-04-01 09:04:49 +0400

    refactor - UI - Remove custom.css

--- YSI.CurseOfSilverCrown.Web/ClientApp/src/App.tsx
+++ YSI.CurseOfSilverCrown.Web/ClientApp/src/App.tsx
@@ -3,8 +3,6 @@ import { Route, Routes } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 
-import './custom.css'
-
 export default () => (
     <Layout>     
         <Routes>
