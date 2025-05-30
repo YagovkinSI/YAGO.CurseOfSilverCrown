d78266c9cf8b17f141fdb2605dfbffa29149be38
YagovkinSI <sorpionupgrade@mail.ru>
2025-05-30 08:51:50 +0400

    [TASK-0003] Вынес стили App.css в компоненты и общий файл стилей, установил styled-components

--- src/Host/ClientApp/src/App.tsx
+++ src/Host/ClientApp/src/App.tsx
@@ -1,12 +1,23 @@
+import styled from 'styled-components';
 import logo from '/favicon.png'
-import './App.css'
+
+const StyledImg = styled.img`
+  width: 60%;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+
+  &:hover {
+    filter: drop-shadow(0 0 2em #b2b448aa);
+  }
+`;
 
 function App() {
   return (
     <>
       <div>
         <a href="/">
-          <img src={logo} className="logo" alt="Yago World logo" />
+          <StyledImg src={logo} className="logo" alt="Yago World logo" />
         </a>
       </div>
       <h1>Yago World</h1>
