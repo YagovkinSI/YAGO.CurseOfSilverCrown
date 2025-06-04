2698195beced162a4caafeb14bcddbeb07195f4f
YagovkinSI <55925712+YagovkinSI@users.noreply.github.com>
2025-06-04 22:51:46 +0400

    [TASK-0004] Добавил кнопку переключения между старым и новым дизайном карты (#7)
    * [TASK-0004] Добавил кнопку переключения между старым и новым дизайном карты
    
    * [TASK-0004] Переименовал кнопку в react, добавил наследование от React.FC
    
    ---------
    
    Co-authored-by: YagovkinSI <sorpionupgrade@mail.ru>

---
+++ src/Host/ClientApp/src/features/ReactRazorSwitchButton.tsx
@@ -0,0 +1,11 @@
+import './reactRazorSwitchButton.css';
+
+const ReactRazorSwitchButton: React.FC = () => {
+    return (
+        <a href="/map" className="switch-button">
+            На старую версию
+        </a>
+    );
+};
+
+export default ReactRazorSwitchButton;
