50a7685d142c9bd3bd835e8e33f183b6fd76abba
YagovkinSI <sorpionupgrade@mail.ru>
2025-07-02 20:38:50 +0400

    [TASK-0341] Изменил даты событий на странице событий

--- src/Infrastructure/Helpers/TurnHelper.cs
+++ src/Infrastructure/Helpers/TurnHelper.cs
@@ -1,13 +1,22 @@
-﻿using YAGO.World.Infrastructure.Database.Models.Turns;
+﻿using System;
 
 namespace YAGO.World.Infrastructure.Helpers
 {
     public static class TurnHelper
     {
-        public static string GetName(int turnId)
+        private static readonly DateTime _eraStart = new(2024, 07, 14, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly string[] seasons = new[] { "Зима", "Весна", "Лето", "Осень" };
+
+        public static string GetName(DateTime turnDate, int turnId)
         {
-            var month = 3574 + turnId - 1;
-            return $"{(month / 12) + 1} год, {(month % 12) + 1} месяц (ход {turnId})";
+            var diffDays = (int)(turnDate - _eraStart).TotalDays;
+
+            var year = diffDays / 4 + 1;
+
+            var seasonIndex = diffDays % 4;
+            var season = seasons[seasonIndex];
+
+            return $"{year} год, {season} (ход {turnId})";
         }
     }
 }
