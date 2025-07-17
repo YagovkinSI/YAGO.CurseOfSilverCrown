6273be7beeff6a6668b60219b00949d3d51072aa
YagovkinSI <sorpionupgrade@mail.ru>
2025-07-17 14:14:40 +0400

    [TASK-0339] Удалил страницы Razor

--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -74,7 +74,6 @@ namespace YAGO.World.Host
             }
             else
             {
-                app.UseExceptionHandler("/Error/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
