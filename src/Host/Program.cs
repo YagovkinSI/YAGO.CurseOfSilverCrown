e56190ba604cf5d6df454e5198f723bbdc84ffff
YagovkinSI <sorpionupgrade@mail.ru>
2025-09-27 10:05:38 +0400

    refactor(back): Перенёс Startup.cs в Program.cs как делается в современных приложениях

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,17 +1,96 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using YAGO.World.Application.ApplicationInitializing;
+using YAGO.World.Application.CurrentUsers;
+using YAGO.World.Application.CurrentUsers.Interfaces;
+using YAGO.World.Application.InfrastructureInterfaces.Database;
+using YAGO.World.Host.Middlewares;
+using YAGO.World.Infrastructure;
 
 namespace YAGO.World.Host
 {
     public static class Program
     {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            ConfigureServices(builder.Services, builder.Configuration);
+
+            var app = builder.Build();
+
+            Configure(app);
+
+            app.Run();
+        }
+
+        private static void ConfigureServices(
+            IServiceCollection services, 
+            Microsoft.Extensions.Configuration.ConfigurationManager configuration)
+        {
+            services.AddInfrastructure(configuration);
+
+            AddApplicationServices(services);
+
+            services.AddControllers();
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        private static void AddApplicationServices(IServiceCollection services)
+        {
+            services.AddHostedService<ApplicationInitializeService>();
+
+            services
+                .AddScoped<ICurrentUserService, CurrentUserService>();
+        }
+
+        private static void Configure(WebApplication app)
+        {
+            MigrateDatabse(app.Services);
+
+            //app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            UseApiEndpoints(app);
+
+            UseSpa(app);
+        }
+
+        private static void MigrateDatabse(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+            var migrator = scope.ServiceProvider.GetRequiredService<IDatabaseMigrator>();
+            migrator.Migrate().GetAwaiter().GetResult();
+        }
+
+        private static void UseApiEndpoints(IApplicationBuilder app)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static void UseSpa(IApplicationBuilder app)
+        {
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+            });
+        }
     }
 }
