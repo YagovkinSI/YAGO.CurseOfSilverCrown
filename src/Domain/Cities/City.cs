5914d5a0a3546c4954eac74fb5aa88ef8ebb89d2
YagovkinSI <sorpionupgrade@mail.ru>
2025-10-05 08:00:06 +0400

    feat(city): Сделал доменную модель City, пока без параметров

---
+++ src/Domain/Cities/City.cs
@@ -0,0 +1,42 @@
+﻿using YAGO.World.Domain.Common.Entities;
+
+namespace YAGO.World.Domain.Cities
+{
+    /// <summary>
+    /// Город игрока
+    /// </summary>
+    public class City : IEntity
+    {
+        /// <summary>
+        /// Идентификатор города
+        /// </summary>
+        public long Id { get; }
+
+        /// <summary>
+        /// Идентификатор игрока владельца
+        /// </summary>
+        public long UserId { get; }
+
+        /// <summary>
+        /// Название города
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Описание города
+        /// </summary>
+        public string Descripion { get; }
+
+        public City(
+            long id,
+            long userId,
+            string name,
+            string descripion)
+        {
+            Id = id;
+            UserId = userId;
+            Name = name;
+            Descripion = descripion;
+        }
+    }
+}
