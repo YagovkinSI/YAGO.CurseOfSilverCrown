3cae95e1842db7e095f7cdab389cd9d9a4327993
YagovkinSI <sorpionupgrade@mail.ru>
2026-01-10 15:12:09 +0400

    feat(cycle): Добавил изменения на фронте

--- src/Host/ClientApp/src/entities/MyCycle.ts
+++ src/Host/ClientApp/src/entities/MyCycle.ts
@@ -4,8 +4,19 @@ import { apiRequester } from "../shared/ApiRequester";
 export interface MyCycle {
     id: number,
     colonyId: number,
-    completedUtc: string | undefined;
+    stepNumber: number,
+    runAtUtc: string | undefined;
+    state: CycleState
 }
+
+export const CycleState = {
+    Unknown: 0 as const,
+    Ready: 1 as const,
+    InProgress: 2 as const,
+    Completed: 3 as const,
+} as const;
+
+export type CycleState = typeof CycleState[keyof typeof CycleState]
 
 const extendedApiSlice = apiRequester.injectEndpoints({
     endpoints: (builder) => ({
