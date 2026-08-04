032dbb815038d5cd00aa08a82cd227cb78520dd9
YagovkinSI <sorpionupgrade@mail.ru>
2026-08-04 20:30:31 +0400

    tests(reforms): ДОбавил тест изменения уровня налога

--- YAGO.World.Tests/Domain/Entities/Buildings/Calculation/BuildingEconomyTests.cs
+++ YAGO.World.Tests/Domain/Entities/Buildings/Calculation/BuildingEconomyTests.cs
@@ -16,7 +16,34 @@ namespace YAGO.World.Tests.Domain.Entities.Buildings.Calculation
             var report = economy.GetReport();
 
             //Assert
-            var reportStr = report.ToString();
+            Assert.True(report.LaborCosts > 750 && report.LaborCosts < 1250);
+            Assert.True(report.AverageSalary > 7 && report.AverageSalary < 13);
+            Assert.True(report.GrossProfit > 1000 && report.LaborCosts < 2000);
+            Assert.True(report.NetProfit > 750 && report.NetProfit < 1250);
+            Assert.True(report.TaxAmount > 400 && report.TaxAmount < 650);
+        }
+
+
+        [Fact]
+        public void GetReport_Tax30()
+        {
+            //Arrange
+            var laws = new EconomicLaws()
+            {
+                CorporateTaxRate = 30
+            };
+            var type = new BuildingTypeSettings();
+            var economy = new BuildingEconomy(10000, laws, type);
+
+            //Act
+            var report = economy.GetReport();
+
+            //Assert
+            Assert.True(report.LaborCosts > 750 && report.LaborCosts < 1250);
+            Assert.True(report.AverageSalary > 7 && report.AverageSalary < 13);
+            Assert.True(report.GrossProfit > 1000 && report.LaborCosts < 2000);
+            Assert.True(report.NetProfit > 650 && report.NetProfit < 1100);
+            Assert.True(report.TaxAmount > 550 && report.TaxAmount < 800);
         }
     }
 }
