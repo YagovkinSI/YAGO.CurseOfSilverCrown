ac98d452019b57d695babff0cad73613dc70b652
YagovkinSI <sorpionupgrade@mail.ru>
2026-08-13 21:47:15 +0400

    fix(api): Попарвил BaseUrl

--- src/Host/ClientApp/src/shared/api/baseUrl.ts
+++ src/Host/ClientApp/src/shared/api/baseUrl.ts
@@ -1,3 +1,3 @@
 //export const baseUrl: string = 'https://localhost:5001/api';
-//const baseUrl : string = 'http://localhost/api';
-const baseUrl : string = 'https://yagoworld.ru/api'
+//export const baseUrl : string = 'http://localhost/api';
+export const baseUrl : string = 'https://yagoworld.ru/api'
