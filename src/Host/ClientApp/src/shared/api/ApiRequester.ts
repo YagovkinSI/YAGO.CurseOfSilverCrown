2f183ee582f4134862edb287c2ddcec20be2b8ac
YagovkinSI <sorpionupgrade@mail.ru>
2026-08-13 08:41:50 +0400

    refactor(api): Вынес urls в отдельный файл, для гибкости

--- src/Host/ClientApp/src/shared/api/ApiRequester.ts
+++ src/Host/ClientApp/src/shared/api/ApiRequester.ts
@@ -1,8 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-//const baseUrl : string = 'https://localhost:5001/api';
-//const baseUrl : string = 'http://localhost/api';
-const baseUrl : string = 'https://yagoworld.ru/api'
+import { baseUrl } from './baseUrl';
 
 const tagTypes = ['UserPrivate', 'MyColony', 'MyTurn', 'DecreeDetails', 'MyBuildings', 'ReformDetails'] as const;
 export type TagType = typeof tagTypes[number];
---
+++ src/Host/ClientApp/src/shared/api/baseUrl.ts
@@ -0,0 +1,3 @@
+//export const baseUrl: string = 'https://localhost:5001/api';
+//const baseUrl : string = 'http://localhost/api';
+const baseUrl : string = 'https://yagoworld.ru/api'
